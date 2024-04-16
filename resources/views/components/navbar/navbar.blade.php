72d1abe5bd65975063c026e7f11ff107440fb68f
rubenrodliz <rubenrodliz@gmail.com>
2024-04-16 16:30:03 +0200

    Place correctly profile item

--- resources/views/components/navbar/navbar.blade.php
+++ resources/views/components/navbar/navbar.blade.php
@@ -1,21 +1,23 @@
 <header class="w-[85px] bg-primary">
-    <nav class="h-screen flex flex-col items-center py-[40px]">
-        <div class="w-14 h-14 rounded-full">
-            <a href="/">
-                <img src="{{ asset('images/logo.svg') }}" alt="BuyWards Logo">
-            </a>
+    <nav class="h-screen flex flex-col justify-between items-center py-[40px]">
+        <div class="grid place-items-center">
+            <div class="w-14 h-14 rounded-full">
+                <a href="/">
+                    <img src="{{ asset('images/logo.svg') }}" alt="BuyWards Logo">
+                </a>
+            </div>
+
+            <x-navbar.separator />
+
+            <ul class="flex flex-col gap-[35px]">
+                <x-navbar.nav-link :icon="'home'" :href="'home'" />
+                <x-navbar.nav-link :icon="'receipt'" :href="'tierlist'" />
+                <x-navbar.nav-link :icon="'shield'" :href="'champs'" />
+                <x-navbar.nav-link :icon="'emoji_events'" :href="'leaderboard'" />
+                <x-navbar.nav-link :icon="'settings'" :href="'settings'" />
+            </ul>
         </div>
 
-        <x-navbar.separator />
-
-        <ul class="flex flex-col gap-[35px]">
-            <x-navbar.nav-link :icon="'home'" :href="'home'" />
-            <x-navbar.nav-link :icon="'receipt'" :href="'tierlist'" />
-            <x-navbar.nav-link :icon="'shield'" :href="'champs'" />
-            <x-navbar.nav-link :icon="'emoji_events'" :href="'leaderboard'" />
-            <x-navbar.nav-link :icon="'settings'" :href="'settings'" />
-        </ul>
-        
         <div class="grid place-items-center">
             <x-navbar.separator />
             <x-navbar.icon-link :icon="'person'" :href="'profile'" />
