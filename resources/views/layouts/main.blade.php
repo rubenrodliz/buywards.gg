08dae6f456602170868a8caa418a6947c7366588
rubenrodliz <rubenrodliz@gmail.com>
2024-04-17 15:14:31 +0200

    Feat(Main Layout): Add scripts section to layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -15,6 +15,8 @@
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    @yield('scripts')
 </head>
 <body class="font-sans antialiased">
     <div class="min-h-screen flex">
