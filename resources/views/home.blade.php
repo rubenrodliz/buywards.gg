b7c9b81e3ef7d47301514db2b09943f2327b05c4
rubenrodliz <rubenrodliz@gmail.com>
2024-04-17 15:15:17 +0200

    Feat(Home Page): Add scripts section to link home.css file + set background-image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,8 @@
 <x-main-layout>
+    @section('scripts')
+        @vite(['resources/css/home.css'])
+    @endsection
+    
     <h2 class="font-semibold text-xl leading-tight">
         Ejemplo de uso de la plantilla principal
     </h2>
