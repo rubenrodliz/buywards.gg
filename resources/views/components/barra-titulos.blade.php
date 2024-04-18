cafa7abb1528181602985f326815cedc1de12c70
davidpqdaw <144946182+davidpqdaw@users.noreply.github.com>
2024-04-18 15:38:45 +0200

    titulos con la linea

---
+++ resources/views/components/barra-titulos.blade.php
@@ -0,0 +1,1 @@
+<div class="w-[80%] border-l-2 border-solid border-primary h-[24px]"><h3 class="ml-[10px]"> {{$titulo}} </h3></div>
--- resources/views/components/rendiment.blade.php
+++ resources/views/components/rendiment.blade.php
@@ -1,3 +1,14 @@
-<div>
-    <div class="w-[200px] h-[200px] border-[#17AF9D] border-solid"></div>
+{{-- <div {{ $attibutes->merge(['class'=>"w-[390px] h-[381px] border-primary border-solid border-[1px] rounded-[10px] flex flex-col m-[200px] bg-dark"]) }}> --}}
+<div class="w-[390px] h-[381px] border-primary border-solid border-[1px] rounded-[10px] flex flex-col m-[200px] bg-dark">
+    <div class="w-[94%] h-[10%] my-[3%] mx-[5%]">
+        @php
+            $titulo="Rendimiento";
+        @endphp
+        @include("components.barra-titulos")
+    </div>
+    <div class="w-[94%] h-[12%] my-[3%] mx-[5%] flex">
+        <div class="w-[30%] h-[50px] mr-[3%] rounded-[10px] border-2 border-solid border-primary bg-primary text-[15px] flex items-center text-center justify-center"><p>Total</p></div>
+        <div class="w-[30%] h-[50px] mr-[3%] rounded-[10px] border-2 border-solid border-primary text-[15px] flex items-center text-center justify-center"><p class="text-primary">SoloQ</p></div>
+        <div class="w-[30%] h-[50px] mr-[3%] rounded-[10px] border-2 border-solid border-primary text-[15px] flex items-center text-center justify-center"><p class="text-primary">Flex</p></div>
+    </div>
 </div>
