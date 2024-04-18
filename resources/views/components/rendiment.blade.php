2c9962e7b23f6b70e3890099c2edd463925226f4
davidpqdaw <144946182+davidpqdaw@users.noreply.github.com>
2024-04-18 15:41:37 +0200

    componente bien

--- resources/views/components/rendiment.blade.php
+++ resources/views/components/rendiment.blade.php
@@ -2,13 +2,14 @@
 <div class="w-[390px] h-[381px] border-primary border-solid border-[1px] rounded-[10px] flex flex-col m-[200px] bg-dark">
     <div class="w-[94%] h-[10%] my-[3%] mx-[5%]">
         @php
-            $titulo="Rendimiento";
+            $titulo="a";
         @endphp
-        @include("components.barra-titulos")
+        <x-barra-titulos titulo="A" />
+        {{-- <div class="w-[80%] border-l-2 border-solid border-primary h-[24px]"><h3 class="ml-[10px]"> Rendimiento </h3></div> --}}
     </div>
     <div class="w-[94%] h-[12%] my-[3%] mx-[5%] flex">
         <div class="w-[30%] h-[50px] mr-[3%] rounded-[10px] border-2 border-solid border-primary bg-primary text-[15px] flex items-center text-center justify-center"><p>Total</p></div>
-        <div class="w-[30%] h-[50px] mr-[3%] rounded-[10px] border-2 border-solid border-primary text-[15px] flex items-center text-center justify-center"><p class="text-primary">SoloQ</p></div>
-        <div class="w-[30%] h-[50px] mr-[3%] rounded-[10px] border-2 border-solid border-primary text-[15px] flex items-center text-center justify-center"><p class="text-primary">Flex</p></div>
+        <div class="w-[30%] h-[50px] mr-[3%] rounded-[10px] border-2 border-solid border-primary text-[15px] flex items-center text-center justify-center"><p>SoloQ</p></div>
+        <div class="w-[30%] h-[50px] mr-[3%] rounded-[10px] border-2 border-solid border-primary text-[15px] flex items-center text-center justify-center"><p>Flex</p></div>
     </div>
 </div>
