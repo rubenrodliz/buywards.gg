309a988ee65f8660245349fd24c609422ec7395a
davidpqdaw <144946182+davidpqdaw@users.noreply.github.com>
2024-04-22 16:29:20 +0200

    summonerdev igual a summoner

--- resources/views/summonerdev.blade.php
+++ resources/views/summonerdev.blade.php
@@ -1,16 +1,34 @@
+
 <x-main-layout>
     @section('scripts')
         @vite(['resources/css/home.css'])
     @endsection
 
     <main class="flex flex-1 flex-col items-center p-[40px]">
-        <div>
-            <x-rendiment/>
-            <x-porcentajeWinrate/>
+        <div class="flex self-end">
+            <a href="#" class="font-bold text-2xl bg-primary rounded-lg py-2 px-4">
+                Login
+            </a>
         </div>
+
+        <div class="w-[1300px] mt-7">
+            <div class="flex justify-between items-center">
+
+            </div>
+            <div class="grid grid-cols-12 gap-5">
+                    <x-summoner-league class="col-start-1 col-span-3 row-start-1"/>
+                    <x-summoner-league class="col-start-1 col-span-3 row-start-2"/>
+                    <x-rendiment class="col-start-1 col-span-3 row-start-3"/>
+                    <x-porcentajeWinrate class="col-start-4 col-span-9 row-start-1"/>
+
+
+            </div>
+
+        </div>
+
+
     </main>
 
 
 </x-main-layout>
-
 
