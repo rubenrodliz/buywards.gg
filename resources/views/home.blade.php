4e9a415cf81fcf80900d81d2beb6a90ac6ba3765
rubenrodliz <rubenrodliz@gmail.com>
2024-04-25 15:15:55 +0200

    Add latest champions section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,14 +3,14 @@
         @vite(['resources/css/home.css'])
     @endsection
 
-    <main class="flex flex-1 flex-col items-center p-[40px]">
+    <main class="flex flex-1 flex-col items-center p-11">
         <div class="flex self-end">
             <a href="#" class="font-bold text-2xl bg-primary rounded-lg py-2 px-4">
                 Login
             </a>
         </div>
 
-        <div class="w-[875px] mt-7">
+        <div class="w-[875px] mt-6">
             <div class="flex justify-between items-center">
                 <img src="{{ asset('images/logo_text.svg') }}" alt="Buywards logo">
                 <img src="{{ asset('images/League_of_Legends.svg') }}" alt="League of Legends logo">
@@ -19,8 +19,15 @@
             <x-searchbar.searchbar />
         </div>
 
-        <div class="mt-14">
-            <h4 id="latest-champions" class="font-bold text-bg_light text-4xl">Latest champions</h4>
+        <div class="mt-16">
+            <div>
+                <h4 id="latest-champions" class="font-bold text-bg_light text-4xl text-center">Latest champions</h4>
+            </div>
+            <div class="latests-champions flex justify-between gap-16 mt-4">
+                <x-champion-card :champion="'Smolder'" />
+                <x-champion-card :champion="'Hwei'" />
+                <x-champion-card :champion="'Briar'" />
+            </div>
         </div>
     </main>
 
