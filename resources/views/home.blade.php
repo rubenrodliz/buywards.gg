49ef06b9e3b1515be5ffcd652fa4ee52b4ecd032
rubenrodliz <rubenrodliz@gmail.com>
2024-04-25 15:16:44 +0200

    Change divs to section for better semantic

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,22 +4,22 @@
     @endsection
 
     <main class="flex flex-1 flex-col items-center p-11">
-        <div class="flex self-end">
+        <section class="flex self-end">
             <a href="#" class="font-bold text-2xl bg-primary rounded-lg py-2 px-4">
                 Login
             </a>
-        </div>
+        </section>
 
-        <div class="w-[875px] mt-6">
+        <section class="w-[875px] mt-6">
             <div class="flex justify-between items-center">
                 <img src="{{ asset('images/logo_text.svg') }}" alt="Buywards logo">
                 <img src="{{ asset('images/League_of_Legends.svg') }}" alt="League of Legends logo">
             </div>
 
             <x-searchbar.searchbar />
-        </div>
+        </section>
 
-        <div class="mt-16">
+        <section class="mt-16">
             <div>
                 <h4 id="latest-champions" class="font-bold text-bg_light text-4xl text-center">Latest champions</h4>
             </div>
@@ -28,7 +28,7 @@
                 <x-champion-card :champion="'Hwei'" />
                 <x-champion-card :champion="'Briar'" />
             </div>
-        </div>
+        </section>
     </main>
 
     
