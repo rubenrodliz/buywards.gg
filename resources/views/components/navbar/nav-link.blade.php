d94dba3031761ecf56f336294b7c0ba393b4ed0b
rubenrodliz <rubenrodliz@gmail.com>
2024-04-29 16:43:40 +0200

    Fix(Navbar - Navlink): Improve navlink and show active page

--- resources/views/components/navbar/nav-link.blade.php
+++ resources/views/components/navbar/nav-link.blade.php
@@ -1,9 +1,17 @@
-@props(['icon', 'size' => '35', 'href'])
+@props(['icon', 'active' => false])
 
 @php
     use App\View\Components\Navbar\IconLink;
 @endphp
 
-<li class="w-[{{ $size }}px] h-[{{ $size }}px]">
-    <x-navbar.icon-link :icon="$icon" :href="$href" :size="$size" />
+<li class="list-none">
+    <a {{ $attributes }}>
+        <span @class([
+            'material-icons rounded-[10px] text-[35px] p-[2px]',
+            'text-primary bg-bg_dark' => $active,
+            'text-white' => !$active,
+        ]) class="" aria-current="page">
+            {{ $icon }}
+        </span>
+    </a>
 </li>
--- resources/views/components/navbar/navbar.blade.php
+++ resources/views/components/navbar/navbar.blade.php
@@ -3,23 +3,28 @@
         <div class="grid place-items-center">
             <div class="w-14 h-14 rounded-full">
                 <a href="/">
-                    <img src="{{ asset('images/logo.svg') }}" alt="BuyWards Logo">
+                    <img src="{{ asset('images/logo.svg') }}" alt="BuyWards Logo" class="aspect-square">
                 </a>
             </div>
 
             <x-navbar.separator />
 
+            {{-- 
+                ToDo: Cuando se vayan desarrollando las demás páginas
+                se debe de cambiar el href de los links para que redirijan
+                a las páginas correspondientes.
+             --}}
             <ul class="flex flex-col gap-[35px]">
-                <x-navbar.nav-link :icon="'home'" :href="'/'" />
-                <x-navbar.nav-link :icon="'receipt'" :href="'tierlist'" />
-                <x-navbar.nav-link :icon="'shield'" :href="'champs'" />
-                <x-navbar.nav-link :icon="'emoji_events'" :href="'leaderboard'" />
+                <x-navbar.nav-link :icon="'home'" :active="request()->is('/')" href="/"/>
+                <x-navbar.nav-link :icon="'receipt'" :action="'tierlist'" />
+                <x-navbar.nav-link :icon="'shield'" :action="'champs'" />
+                <x-navbar.nav-link :icon="'emoji_events'" :action="'leaderboard'" />
             </ul>
         </div>
 
         <div class="grid place-items-center">
             <x-navbar.separator />
-            <x-navbar.icon-link :icon="'person'" :href="'profile'" />
+            <x-navbar.nav-link :icon="'person'" :active="request()->is('/profile')" href="/profile" />
         </div>
     </nav>
 </header>
