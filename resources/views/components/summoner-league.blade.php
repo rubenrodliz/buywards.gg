0454125a07f2140d8c73196574bf03a88724f1f3
OkasarRP <oskyrp2000@gmail.com>
2024-04-30 15:38:12 +0200

    commit 29-04-2024

--- resources/views/components/summoner-league.blade.php
+++ resources/views/components/summoner-league.blade.php
@@ -1,10 +1,7 @@
 
-
-
-
-<div {{ $attributes->merge(['class' => 'grid grid-cols-3 grid-rows-3 border border-red-500 border-solid rounded-lg p-4 bg-primary h-[130px]']) }}>
-    <div class="col-start-1 col-span-3 row-start-1">Clasificatoria Solo/Duo</div>
-    <div class="col-start-1 col-span-2 row-start-2">img</div>
+<div {{ $attributes->merge(['class' => 'grid grid-cols-3 grid-rows-3 text-[#ffffff] border border-primary border-solid rounded-lg p-4 bg-transparent h-[130px]']) }}>
+    <div class="col-start-1 col-span-3 row-start-1 "><x-barra-titulos titulo="Clasificatoria Solo/Duo" /></div>
+    <div class="col-start-1 col-span-2 row-start-2 ">img</div>
     <div class="col-start-2 col-span-2 row-start-2">Diamante 4</div>
     <div class="col-start-2 col-span-2 row-start-3">40LP</div>
     <div class="col-start-3 col-span-3 row-start-2 text-right">47W 40L</div>
