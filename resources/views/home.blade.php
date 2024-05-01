47f0c97790f56495fa0694331a51a1fb1dc77ddc
Goizane <goizaneolaneta@gmail.com>
2024-05-01 22:34:44 +0200

    Add AuthLoginDropdownButton component and its blade view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,29 +4,7 @@
     @endsection
 
     <main class="flex flex-1 flex-col items-center p-[1%]">
-        @if(auth()->check())
-            <section class="flex self-end">
-                <div class="relative">
-                    <button class="font-bold text-xl bg-primary rounded-lg py-2 px-4 dropdown-button">
-                        {{ auth()->user()->name }}
-                    </button>
-                    <div class="absolute right-0 mt-2 w-48 bg-white rounded-lg bg-bg_light text-center hidden dropdown-menu">
-                        <a href="{{ route('profile.edit') }}" class="block px-4 py-2 text-gray-800 hover:bg-secondary rounded-t-lg hover:font-semibold">Editar perfil</a>
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-                            <button type="submit" class="block w-full px-4 py-2 text-gray-800 text-center text-error hover:text-text_light hover:bg-error rounded-b-lg hover:font-semibold">Cerrar sesión</button>
-                        </form>
-                    </div>
-                </div>
-            </section>
-        @else
-            <section class="flex self-end">
-                <a href="{{ route('login') }}" class="font-bold text-xl bg-primary rounded-lg py-2 px-4">
-                    Login
-                </a>
-            </section>
-        @endif
-
+        <x-auth-login-dropdown-button />
 
         <section class="w-[875px] mt-6">
             <div class="flex justify-between items-center">
