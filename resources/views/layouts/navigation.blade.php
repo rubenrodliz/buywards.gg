233dc1674ec090fcb16b2292c52192e7910c7747
Goizane <goizaneolaneta@gmail.com>
2024-05-01 22:09:05 +0200

    Fix navigation layout and styles in navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+{{-- <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
@@ -12,9 +12,9 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-navbar-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    <x-navbar.nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
-                    </x-navbar-nav-link>
+                    </x-navbar.nav-link>
                 </div>
             </div>
 
@@ -98,3 +98,4 @@
         </div>
     </div>
 </nav>
+--}}
