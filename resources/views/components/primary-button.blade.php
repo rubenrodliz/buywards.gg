47bb6f52db1b12ec52bdda940a90b64da6c7dc5f
rubenrodliz <rubenrodliz@gmail.com>
2024-05-02 20:07:45 +0200

    Remove mt-8 for primary button

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center justify-center px-4 w-full h-full mt-8 rounded-md font-semibold text-lg transition ease-in-out duration-150']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center justify-center px-4 w-full h-full rounded-md font-semibold text-lg transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </button>
