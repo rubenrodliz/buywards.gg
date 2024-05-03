80b23c7c71f1290b1243bf5d7e42ab8ee32d85cb
Goizane <goizaneolaneta@gmail.com>
2024-05-03 03:58:56 +0200

    Add champPerformance component for displaying champion performance

---
+++ resources/views/components/champ-performance.blade.php
@@ -0,0 +1,31 @@
+@props(['champName', 'champKDA', 'champWinRate', 'champWins', 'champLoses', 'champImage'])
+
+<div class="h-full"><img src="{{ $champImage }}" alt="{{ $champName }} lol character" class="w-full rounded-[10px]"></div>
+<div class="h-full w-[50%] pl-[20px]">
+    <p class="">{{ $champName ?? ''}}</p>
+    <p class="
+        @if($champKDA < 3)
+            {{ 'text-kda_red' }}
+        @elseif($champKDA >= 3 && $champKDA <= 5)
+            {{ 'text-kda_blue' }}
+        @else
+            {{ 'text-kda_orange' }}
+        @endif
+    ">
+        {{ $champKDA }}
+    </p>
+</div>
+<div class="h-full w-[35%] text-end">
+    <p class="
+        @if($champWinRate < 50)
+            {{ 'text-kda_red' }}
+        @elseif($champWinRate >= 50 && $champWinRate <= 60)
+            {{ 'text-kda_blue' }}
+        @else
+            {{ 'text-kda_orange' }}
+        @endif
+    ">
+        {{ $champWinRate }}%
+    </p>
+    <p class="text-sm">{{ $champWins }}V - {{ $champLoses }}D</p>
+</div>
