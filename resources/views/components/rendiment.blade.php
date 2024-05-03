584c479175680a7ee8893a94b57a7008973699d2
Goizane <goizaneolaneta@gmail.com>
2024-05-03 03:59:39 +0200

    Changed some styles to be readen more clearly at a first glance

--- resources/views/components/rendiment.blade.php
+++ resources/views/components/rendiment.blade.php
@@ -1,33 +1,25 @@
-<div {{ $attributes->merge(['class'=>"w-[100%] h-[381px] border-primary border-solid border-[1px] rounded-[10px] flex flex-col bg-transparent text-[#FFFFFF]"]) }}>
+<div {{ $attributes->merge(['class'=>"py-4 px-4 w-[100%] border-primary border-solid border-[1px] rounded-[10px] flex flex-col bg-transparent text-text_light"]) }}>
 {{-- <div class="w-[390px] h-[381px] border-primary border-solid border-[1px] rounded-[10px] flex flex-col bg-transparent  text-white"> --}}
-    <div class="w-[94%] h-[10%] my-[3%] mx-[5%]">
+    <div class="h-[10%] mb-4">
         <x-barra-titulos titulo="Rendimiento" />
     </div>
-    <div class="w-[94%] h-[12%] my-[3%] mx-[5%] flex">
-        <div class="w-[30%] h-[50px] mr-[3%] rounded-[10px] border-2 border-solid border-primary bg-primary text-[15px] flex items-center text-center justify-center"><p>Total</p></div>
-        <div class="w-[30%] h-[50px] mr-[3%] rounded-[10px] border-2 border-solid border-primary text-[15px] flex items-center text-center justify-center text-primary hover:bg-primary hover:text-[#FFFFFF]"><p>SoloQ</p></div>
-        <div class="w-[30%] h-[50px] mr-[3%] rounded-[10px] border-2 border-solid border-primary text-[15px] flex items-center text-center justify-center text-primary hover:bg-primary hover:text-[#FFFFFF]"><p>Flex</p></div>
+    <div class="h-[12%] flex flex-row justify-between mb-4 w-full">
+        <div class="py-2 w-[25%] rounded-[10px] border-2 border-solid border-primary bg-primary text-md flex items-center text-center justify-center text-text_dark font-semibold"><p>Total</p></div>
+        <div class="py-2 w-[25%] rounded-[10px] border-2 border-solid border-primary text-md flex items-center text-center justify-center text-primary hover:bg-primary hover:text-text_dark hover:font-semibold hover:cursor-pointer"><p>SoloQ</p></div>
+        <div class="py-2 w-[25%] rounded-[10px] border-2 border-solid border-primary text-md flex items-center text-center justify-center text-primary hover:bg-primary hover:text-text_dark hover:font-semibold hover:cursor-pointer"><p>Flex</p></div>
     </div>
-    <div class="h-[70%] w-[90%] self-center flex flex-col">
-        <div class="h-[20%] w-[100%] my-[1.5%] flex">
-            <div class="h-full w-[15%]"><img src="{{asset('images/kaisa.png')}}" alt="kai'sa lol character" class="w-full rounded-[10px]"></div>
-            <div class="h-full w-[50%] pl-[20px]"><p class="text-[15px]">Kai'sa</p><p class="text-[15px] text-[#2D62D7]">3.69 KDA</p></div>
-            <div class="h-full w-[35%] text-end"><p class="text-[15px] text-[#2D62D7]">52%</p><p class="text-[15px]">8V - 7D</p></div>
+    <div class="self-center flex flex-col w-full">
+        <div class="w-full h-fit mb-4 flex justify-between items-center font-semibold">
+            <x-champ-performance champName="Kai'sa" champKDA="3.69" champWinRate="52" champWins="8" champLoses="7" champImage="{{asset('images/kaisa.png')}}" />
         </div>
-        <div class="h-[20%] w-[100%] my-[1.5%] flex">
-            <div class="h-full w-[15%]"><img src="{{asset('images/kaisa.png')}}" alt="kai'sa lol character" class="w-full rounded-[10px]"></div>
-            <div class="h-full w-[50%] pl-[20px]"><p class="text-[15px]">Kai'sa</p><p class="text-[15px] text-[#F54B4E]">0.8 KDA</p></div>
-            <div class="h-full w-[35%] text-end"><p class="text-[15px] text-[#F54B4E]">30%</p><p class="text-[15px]">8V - 7D</p></div>
+        <div class="w-full h-fit mb-4 flex justify-between items-center font-semibold">
+            <x-champ-performance champName="Kai'sa" champKDA="0.8" champWinRate="30" champWins="8" champLoses="7" champImage="{{asset('images/kaisa.png')}}" />
         </div>
-        <div class="h-[20%] w-[100%] my-[1.5%] flex">
-            <div class="h-full w-[15%]"><img src="{{asset('images/kaisa.png')}}" alt="kai'sa lol character" class="w-full rounded-[10px]"></div>
-            <div class="h-full w-[50%] pl-[20px]"><p class="text-[15px]">Kai'sa</p><p class="text-[15px] text-[#F69601]">7.29 KDA</p></div>
-            <div class="h-full w-[35%] text-end"><p class="text-[15px] text-[#F69601]">70%</p><p class="text-[15px]">8V - 7D</p></div>
+        <div class="w-full h-fit mb-4 flex justify-between items-center font-semibold">
+            <x-champ-performance champName="Kai'sa" champKDA="2.49" champWinRate="62" champWins="8" champLoses="7" champImage="{{asset('images/kaisa.png')}}" />
         </div>
-        <div class="h-[20%] w-[100%] my-[1.5%] flex">
-            <div class="h-full w-[15%]"><img src="{{asset('images/kaisa.png')}}" alt="kai'sa lol character" class="w-full rounded-[10px]"></div>
-            <div class="h-full w-[50%] pl-[20px]"><p class="text-[15px]">Kai'sa</p><p class="text-[15px] text-[#2D62D7]">2.49 KDA</p></div>
-            <div class="h-full w-[35%] text-end"><p class="text-[15px] text-[#2D62D7]">62%</p><p class="text-[15px]">8V - 7D</p></div>
+        <div class="w-full h-fit flex justify-between items-center font-semibold">
+            <x-champ-performance champName="Kai'sa" champKDA="6.77" champWinRate="52" champWins="8" champLoses="7" champImage="{{asset('images/kaisa.png')}}" />
         </div>
     </div>
 </div>
