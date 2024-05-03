7f8cdc4cd55cca4b6e8bb3dbbd7e6d24f8542cb6
Goizane <goizaneolaneta@gmail.com>
2024-05-03 06:09:02 +0200

    Refactor porcentajeWinrate component and blade template

--- app/View/Components/porcentajeWinrate.php
+++ app/View/Components/porcentajeWinrate.php
@@ -21,6 +21,6 @@ class porcentajeWinrate extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.porcentajeWinrate');
+        return view('components.porcentaje-winrate');
     }
 }
---
+++ resources/views/components/porcentaje-winrate.blade.php
@@ -0,0 +1,33 @@
+@props(['class'])
+
+<div class="border-solid border-2 border-primary rounded-[10px] flex flex-col bg-transparent text-text_light bg-opacity-60 bg-bg_dark {{ $class }}">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    {{-- <div class="w-[1100px] h-[165px] border-primary border-solid border-[1px] rounded-[10px] flex flex-col bg-transparent text-white"> --}}
+    <div class="flex justify-center p-4">
+        <div class="flex flex-row justify-between w-[50%]">
+            <x-performance-selector-button isSelected="true" name="Total" />
+            <x-performance-selector-button name="SoloQ" />
+            <x-performance-selector-button name="Flex" />
+        </div>
+        <div class="w-[50%] flex justify-end">
+            <x-search-champion />
+        </div>
+
+    </div>
+    <hr class="text-primary">
+    <div class="flex p-4 justify-between">
+        <div class="flex justify-between items-center w-[30%]">
+            <div class="flex gap-8">
+                <canvas id="PorcentajeWinRate" width="75" height="70"></canvas>
+                <x-historial-wins-loses wins="5" loses="8" />
+            </div>
+
+            <x-historial-k-d-a kills="5" deaths="4" assists="10" />
+        </div>
+        <div class="flex justify-end w-[45%] justify-between items-center">
+            <x-historial-champ-stats champName="Kai'sa" champKDA="5,43" champWinRate="50" champWins="5" champLoses="5" champImage="kaisa" />
+            <x-historial-champ-stats champName="Kai'sa" champKDA="5,43" champWinRate="30" champWins="5" champLoses="5" champImage="kaisa" />
+            <x-historial-champ-stats champName="Kai'sa" champKDA="5,43" champWinRate="70" champWins="5" champLoses="5" champImage="kaisa" />
+        </div>
+    </div>
+</div>
