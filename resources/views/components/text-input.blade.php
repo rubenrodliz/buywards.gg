f4af3310357fd2323a283a8e0484610909e6d393
rubenrodliz <rubenrodliz@gmail.com>
2024-05-03 10:33:41 +0200

    Remove focus effect for text-input component

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'rounded-md shadow-sm rounded-md shadow-sm focus:border-bg_light']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'rounded-md shadow-sm rounded-md shadow-sm']) !!}>
