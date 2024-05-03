694243fc407de9cd48952a6e1f15af469317d587
rubenrodliz <rubenrodliz@gmail.com>
2024-05-03 11:42:37 +0200

    Feat(SummonerLeague Component): Add tier parameter, show rank information

--- resources/views/components/summoner-league.blade.php
+++ resources/views/components/summoner-league.blade.php
@@ -1,13 +1,15 @@
-@props(['titulo', 'class', 'rank', 'lp', 'wins', 'losses', 'winrate'])
+@props(['titulo', 'class', 'rank', 'tier', 'lp', 'wins', 'losses', 'winrate'])
 
 <div class="flex flex-col text-text_light border-2 border-primary border-solid rounded-lg p-4 bg-transparent h-[130px] bg-opacity-60 bg-bg_dark {{ $class }}">
     <div class="flex-1">
         <x-barra-titulos titulo="{{ $titulo }}" />
     </div>
     <div class="flex flex-row flex-wrap justify-center items-center">
-        <img src="{{ asset('images/ranks/' . substr(strtolower($rank), 0, -2)  . '.webp') }}" alt="league rank" class="w-[20%] mr-3">
+        <img src="{{ asset('images/ranks/' . strtolower($rank)  . '.webp') }}" alt="league rank" class="w-[20%] mr-3">
         <div class="flex-1 flex flex-col">
-            <div class="text-xl font-semibold text-{{ substr(strtolower($rank), 0, -2) }}">{{ $rank }}</div>
+            <div class="text-xl font-semibold text-{{ strtolower($rank) }}">
+                {{ $rank }} {{ $tier }}
+            </div>
             <div>{{ $lp }} LP</div>
         </div>
         <div class="flex-1 flex flex-col justify-end">
