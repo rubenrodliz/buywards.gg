96b85afa0f9b7fe31310027e7201a2277dc32930
Goizane <goizaneolaneta@gmail.com>
2024-05-03 03:08:39 +0200

    create top-navbar component

---
+++ resources/views/components/top-navbar.blade.php
@@ -0,0 +1,11 @@
+<div class="w-full bg-bg_dark h-fit drop-shadow-md flex justify-around items-center py-3">
+    <div class="w-[30%] flex justify-start items-center">
+        <img src="{{ asset('images/League_of_Legends.svg') }}" alt="League of Legends logo" class="w-[20%]">
+    </div>
+    <div class="w-[30%] flex justify-center items-center">
+        <x-searchbar.searchbar />
+    </div>
+    <div class="w-[30%] flex justify-end items-center" >
+        <x-auth-login-dropdown-button />
+    </div>
+</div>
