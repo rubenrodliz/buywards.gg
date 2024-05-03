b1356037c6d0662fdb0e48f779fcfee347db0cd4
rubenrodliz <rubenrodliz@gmail.com>
2024-05-03 11:55:17 +0200

    Refactor code adding getKdaClass for clarity

--- resources/views/components/historial-k-d-a.blade.php
+++ resources/views/components/historial-k-d-a.blade.php
@@ -2,17 +2,19 @@
 
 @php
     $kda = ($deaths > 0) ? ($kills + $assists) / $deaths : ($kills + $assists);
+
+    function getKdaClass($kda) {
+        if ($kda < 3) {
+            return 'text-kda_red';
+        } elseif ($kda >= 3 && $kda <= 5) {
+            return 'text-kda_blue';
+        } else {
+            return 'text-kda_orange';
+        }
+    }
 @endphp
 
 <div class="flex flex-col align-middle justify-start">
-    <p class="text-lg font-bold
-        @if($kda < 3)
-            {{ 'text-kda_red' }}
-        @elseif($kda >= 3 && $kda <= 5)
-            {{ 'text-kda_blue' }}
-        @else
-            {{ 'text-kda_orange' }}
-        @endif
-    ">{{ number_format($kda, 2) }} KDA</p>
+    <p class="text-lg font-bold {{ getKdaClass($kda) }}">{{ number_format($kda, 2) }} KDA</p>
     <p class="text-md font-bold">{{ $kills }} / {{ $deaths }} / {{ $assists }}</p>
 </div>
