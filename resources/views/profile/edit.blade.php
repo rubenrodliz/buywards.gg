2bc5ce731d23bfd7250a6bff2ff13a8a0eb39840
Goizane <goizaneolaneta@gmail.com>
2024-05-07 18:19:24 +0200

    Changed the style of the edit profile page, now semanthically matches the style of the app

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,13 +1,13 @@
 <x-auth-layout>
     <!-- Styles -->
     @section('scripts')
-        @vite(['resources/css/profile_edit  .css'])
+        @vite(['resources/css/profile_edit.css'])
     @endsection
 
-    <main class="flex flex-col items-center p-[1%] bg-primary h-fit w-full">
+    <main class="flex flex-col justify-start p-[1%] bg-bg_dark h-[100vh] w-full">
         <!-- Go back -->
         <section class="self-start items-center mb-2">
-            <a class="text-sm text-gray-600 rounded-md hover:font-medium hover:text-text_light" href="{{ URL::to('/')}}">
+            <a class="text-lg text-text_light rounded-md hover:font-medium hover:text-primary" href="{{ URL::to('/')}}">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline-block align-middle mr-1 -mt-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"/>
                 </svg>
@@ -15,8 +15,44 @@
             </a>
         </section>
 
-        @include('profile.partials.update-profile-information-form')
-        @include('profile.partials.update-password-form')
-        @include('profile.partials.delete-user-form')
+        <div class="w-[60vw] h-fit flex flex-col justify-center items-center">
+            <div class="w-full h-fit bg- my-8 rounded-lg bg-primary text-text_dark flex gap-4 justify-center items-center p-4">
+                <img src="{{ asset('images/logo.svg') }}">
+                <div>
+                    <p class="text-4xl">Hi <b>{{ $user->name }}</b>!</p>
+                    <p class="text-xl">Welcome to your profile.</p>
+                </div>
+            </div>
+
+            <!-- Botones para mostrar/ocultar formularios -->
+            <div class="flex justify-center gap-8 mb-4 text-xl">
+                <div class="flex flex-col justify-center items-center gap-2 h-[50px]">
+                    <button id="update-profile-button" class="btn text-text_light hover:text-primary font-bold">Update Profile</button>
+                    <div id="selected-bar-update-profile" class="w-[80%] h-1 bg-primary rounded-lg"></div>
+                </div>
+                <div class="flex flex-col justify-center items-center gap-2 h-[50px]">
+                    <button id="update-password-button" class="btn text-text_light hover:text-primary">Update Password</button>
+                    <div id="selected-bar-update-password" class="w-[80%] h-1 bg-bg_dark rounded-lg"></div>
+                </div>
+                <div class="flex flex-col justify-center items-center gap-2 h-[50px]">
+                    <button id="delete-user-button" class="btn text-text_light hover:text-primary">Delete User</button>
+                    <div id="selected-bar-delete-user" class="w-[80%] h-1 bg-bg_dark rounded-lg"></div>
+                </div>
+            </div>
+
+            <!-- Formularios ocultos -->
+            <div id="update-profile-form" class="w-full">
+                @include('profile.partials.update-profile-information-form')
+            </div>
+            <div id="update-password-form" class="hidden w-full">
+                @include('profile.partials.update-password-form')
+            </div>
+            <div id="delete-user-form" class="hidden  w-full">
+                @include('profile.partials.delete-user-form')
+            </div>
+        </div>
     </main>
+
+    <script src="{{ asset('js/buttonAuthLogic.js') }}"></script>
+
 </x-auth-layout>
--- resources/views/profile/partials/delete-user-form.blade.php
+++ resources/views/profile/partials/delete-user-form.blade.php
@@ -1,21 +1,21 @@
-<section class="space-y-6">
+<section class="space-y-6 text-text_light">
     <header>
-        <h2 class="text-lg font-medium text-gray-900">
+        <h2 class="text-lg font-medium">
             {{ __('Delete Account') }}
         </h2>
 
-        <p class="mt-1 text-sm text-gray-600">
+        <p class="mt-1 text-sm">
             {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.') }}
         </p>
     </header>
 
-    <x-danger-button
+    <x-danger-button class="bg-primary text-text_dark text-sm md:text-lg hover:text-text_light py-2 md:mt-4"
         x-data=""
         x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')"
     >{{ __('Delete Account') }}</x-danger-button>
 
     <x-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
-        <form method="post" action="{{ route('profile.destroy') }}" class="p-6">
+        <form method="post" action="{{ route('profile.destroy') }}" class="p-6 bg-bg_dark border-2 border-primary rounded-lg">
             @csrf
             @method('delete')
 
@@ -34,7 +34,7 @@
                     id="password"
                     name="password"
                     type="password"
-                    class="mt-1 block w-3/4"
+                    class="mt-1 block w-3/4 focus:border-primary block mt-1 w-full placeholder-text_light placeholder:text-sm md:placeholder:text-[1rem] border-primary bg-bg_dark border-1 focus:ring-primary"
                     placeholder="{{ __('Password') }}"
                 />
 
@@ -42,11 +42,11 @@
             </div>
 
             <div class="mt-6 flex justify-end">
-                <x-secondary-button x-on:click="$dispatch('close')">
+                <x-secondary-button class="bg-primary text-text_dark text-sm md:text-lg hover:text-text_light py-2 md:mt-4" x-on:click="$dispatch('close')">
                     {{ __('Cancel') }}
                 </x-secondary-button>
 
-                <x-danger-button class="ms-3">
+                <x-danger-button class="ms-3 bg-error text-text_dark text-sm md:text-lg hover:text-text_light py-2 md:mt-4">
                     {{ __('Delete Account') }}
                 </x-danger-button>
             </div>
