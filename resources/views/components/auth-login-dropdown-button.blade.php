6ff8b553ffe9eba59ade280a0884b73ba88e3806
Ruben <rubenrodliz@gmail.com>
2024-05-08 15:38:17 +0200

    Move dropdownProfile.js to resources/js for matching with Laravel File Structure + implement in component

--- resources/views/components/auth-login-dropdown-button.blade.php
+++ resources/views/components/auth-login-dropdown-button.blade.php
@@ -1,17 +1,18 @@
 @if(auth()->check())
-<section class="flex self-end">
-    <div class="relative">
-        <button class="font-bold text-xl bg-primary rounded-lg py-2 px-4 dropdown-button">
-            {{ auth()->user()->name }}
-        </button>
-        <div class="absolute right-0 mt-2 w-48 bg-white rounded-lg bg-bg_light text-center hidden dropdown-menu">
-            <a href="{{ route('profile.edit') }}" class="block px-4 py-2 text-gray-800 hover:bg-secondary rounded-t-lg hover:font-semibold">Editar perfil</a>
-            <form method="POST" action="{{ route('logout') }}">
-                @csrf
-                <button type="submit" class="block w-full px-4 py-2 text-gray-800 text-center text-error hover:text-text_light hover:bg-error rounded-b-lg hover:font-semibold">Cerrar sesión</button>
-            </form>
+    @vite('resources/js/dropdownProfile.js')
+    <section class="flex self-end">
+        <div class="relative">
+            <button class="font-bold text-xl bg-primary rounded-lg py-2 px-4 dropdown-button">
+                {{ auth()->user()->name }}
+            </button>
+            <div class="absolute right-0 mt-2 w-48 bg-white rounded-lg bg-bg_light text-center hidden dropdown-menu">
+                <a href="{{ route('profile.edit') }}" class="block px-4 py-2 text-gray-800 hover:bg-secondary rounded-t-lg hover:font-semibold">Editar perfil</a>
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button type="submit" class="block w-full px-4 py-2 text-gray-800 text-center text-error hover:text-text_light hover:bg-error rounded-b-lg hover:font-semibold">Cerrar sesión</button>
+                </form>
+            </div>
         </div>
-    </div>
 </section>
 @else
     <section class="flex self-end">
