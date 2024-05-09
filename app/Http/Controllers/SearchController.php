2ea0e27becae15cd1d1a26e293f34fade8e022b1
Ruben <rubenrodliz@gmail.com>
2024-05-09 16:54:29 +0200

    Feat(SearchController): Make searchData method to get the requests summoner

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,36 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Services\RiotService;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
     public function searchSummoner(RiotService $riotService, Request $request) {
         // Get the summoner name and region from the request
-        $summoner = trim($request->input('summoner'));
-
-        // Patron de expresion regular para encontrar el tag
-        $tagPattern = '/#(\w{3,5})$/';
-
-        // Buscar coincidencias usando expresiones regulares
-        if (preg_match($tagPattern, $summoner, $matches)) {
-            $tag = $matches[1];
-            // El nombre del invocador será lo que esté antes del tag
-            $summonerName = trim(str_replace("#$tag", "", $summoner));
-        } else {
-            return back()->withErrors(['error' => 'Summoner name and tag format is invalid'], 400);
-        }
-
-        $searchData = [
-            'summoner' => $summonerName,
-            'tag' => $tag,
-            'region' => $request->input('region')
-        ];
+        $searchData = $this->searchData($request->input('summoner'), $request->input('region'));
 
         // Obtain the puuid of the summoner
         $account = $riotService::getAccountByPuuid($searchData['summoner'], $searchData['tag'], $searchData['region']);
-
-        dd($account);
 
         if (!$account) {
             return back()->withErrors(['error' => 'Summoner not found'], 404);
@@ -66,5 +47,22 @@ class SearchController extends Controller
 
         // Implementar procesamiento de datos para enviar solo la información necesaria
         dd($account, $summonerData, $leagueEntries, $matches);
+    }
+    
+    private function searchData (string $summoner, string $region): array|RedirectResponse {
+        $tagPattern = '/#(\w{3,5})$/';
+
+        if (preg_match($tagPattern, $summoner, $matches)) {
+            $tag = $matches[1];
+            $summonerName = trim(str_replace("#$tag", "", $summoner));
+        } else {
+            return back()->withErrors(['error' => 'Summoner name and tag format is invalid'], 400);
+        }
+
+        return [
+            'summoner' => $summonerName,
+            'tag' => $tag,
+            'region' => $region
+        ];
     }
 }
