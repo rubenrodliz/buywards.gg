1dbb7ae78f658e64d4d85dc0784e0fdbf83474ce
Ruben <rubenrodliz@gmail.com>
2024-05-13 16:04:09 +0200

    feat: add responsive behavior and refactor

--- resources/views/components/auth-login-dropdown-button.blade.php
+++ resources/views/components/auth-login-dropdown-button.blade.php
@@ -1,23 +1,25 @@
-@if(auth()->check())
-    @vite('resources/js/dropdownProfile.js')
-    <section class="flex self-end">
+<section class="self-end hidden lg:flex">
+    @if (auth()->check())
+        @vite('resources/js/dropdownProfile.js')
         <div class="relative">
             <button class="font-bold text-xl bg-primary rounded-lg py-2 px-4 dropdown-button">
                 {{ auth()->user()->name }}
             </button>
             <div class="absolute right-0 mt-2 w-48 bg-white rounded-lg bg-bg_light text-center hidden dropdown-menu">
-                <a href="{{ route('profile.edit') }}" class="block px-4 py-2 text-gray-800 hover:bg-secondary rounded-t-lg hover:font-semibold">Editar perfil</a>
+                <a href="{{ route('profile.edit') }}"
+                    class="block px-4 py-2 text-gray-800 hover:bg-secondary rounded-t-lg hover:font-semibold">Editar
+                    perfil</a>
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
-                    <button type="submit" class="block w-full px-4 py-2 text-gray-800 text-center text-error hover:text-text_light hover:bg-error rounded-b-lg hover:font-semibold">Cerrar sesión</button>
+                    <button type="submit"
+                        class="block w-full px-4 py-2 text-gray-800 text-center text-error hover:text-text_light hover:bg-error rounded-b-lg hover:font-semibold">Cerrar
+                        sesión</button>
                 </form>
             </div>
         </div>
-</section>
-@else
-    <section class="flex self-end">
+    @else
         <a href="{{ route('login') }}" class="font-bold text-xl bg-primary rounded-lg py-2 px-4">
             Login
         </a>
-    </section>
-@endif
+    @endif
+</section>
