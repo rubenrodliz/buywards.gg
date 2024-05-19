869d54a55b9f2a929b43fd5cf76a018cefbf0426
OkasarRP <oskyrp2000@gmail.com>
2024-05-19 19:47:46 +0200

    icon z-index correct

--- resources/views/components/top-navbar.blade.php
+++ resources/views/components/top-navbar.blade.php
@@ -1,6 +1,6 @@
 @vite('resources/js/dropdownProfile.js')
 
-<section class="w-full summoner bg-bg_dark h-fit drop-shadow-md flex justify-around items-center py-3 sticky top-0">
+<section class="w-full summoner bg-bg_dark h-fit drop-shadow-md flex justify-around items-center py-3 sticky top-0 z-index">
     <div class="w-[40%] lg:w-[30%] flex ml-[20px] justify-start items-center">
         <img src="{{ asset('images/League_of_Legends.svg') }}" alt="League of Legends logo" class="w-[40%] lg:w-[20%]">
     </div>
