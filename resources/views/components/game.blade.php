d2bdbb95d3952ccd86b54470d324e112a55167a5
rubenrodliz <rubenrodliz@gmail.com>
2024-05-19 23:26:26 +0200

    fix: remove unused code + format

--- resources/views/components/game.blade.php
+++ resources/views/components/game.blade.php
@@ -1,53 +1,62 @@
-@props(["summonerData"])
-@vite(['/resources/css/summoner.css'])
+@props(['summonerData'])
 
-@if (isset($summonerData["game"][10]["ownData"]))
+@if (isset($summonerData['game'][10]['ownData']))
     @php
-        if ($summonerData["game"][10]["ownData"]['win']==true) {
-            $color="kda_blue";
-            $bgColor="bg-kda_blue";
-            $win="Win";
-        }else{
-            $color="kda_red";
-            $bgColor="bg-kda_red";
-            $win="Lose";
+        if ($summonerData['game'][10]['ownData']['win'] == true) {
+            $color = 'kda_blue';
+            $bgColor = 'bg-kda_blue';
+            $win = 'Win';
+        } else {
+            $color = 'kda_red';
+            $bgColor = 'bg-kda_red';
+            $win = 'Lose';
         }
     @endphp
 
-    <div {{ $attributes->merge(['class'=>"bg-opacity-60 bg-bg_dark border-$color border-2 rounded-xl flex justify-between text-$color"]) }}>
-        <div class="w-8 {{$bgColor}} rounded-l-[10px]"></div>
+    <div
+        {{ $attributes->merge(['class' => "bg-opacity-60 bg-bg_dark border-$color border-2 rounded-xl flex justify-between text-$color"]) }}>
+        <div class="w-8 {{ $bgColor }} rounded-l-[10px]"></div>
         <div class="w-[90%] flex justify-around">
             <div class="flex justify-center">
                 <div class="self-center text-text_light text-[16px] font-medium">
-                    <h2 class="text-[20px] font-bold">{{$summonerData['generalGameData']['gameMode']}}</h2>
-                    <p>{{$summonerData['generalGameData']['gameCreation']}}</p>
-                    <hr class="text-{{$color}} my-[10px]">
-                    <p><span class="text-{{$color}}">{{$win}}</span> {{$summonerData['generalGameData']['gameDuration']}}</p>
-                    {{-- <p>{{$data->ownGameInfo->tier}} {{$data->ownGameInfo->rank}}</p> --}}
+                    <h2 class="text-[20px] font-bold">{{ $summonerData['generalGameData']['gameMode'] }}</h2>
+                    <p>{{ $summonerData['generalGameData']['gameCreation'] }}</p>
+                    <hr class="text-{{ $color }} my-[10px]">
+                    <p><span class="text-{{ $color }}">{{ $win }}</span>
+                        {{ $summonerData['generalGameData']['gameDuration'] }}</p>
                 </div>
-                <div class="self-center text-text_light text-[16px] font-medium flex flex-wrap items-center justify-center">
+                <div
+                    class="self-center text-text_light text-[16px] font-medium flex flex-wrap items-center justify-center">
                     <div class="w-[70px] h-[50%]">
-                        <img src="https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/{{$summonerData["game"][10]["ownData"]["championId"]}}.png" alt="" class="rounded-lg">
+                        <img src="https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/{{ $summonerData['game'][10]['ownData']['championId'] }}.png"
+                            alt="" class="rounded-lg">
                     </div>
                     <div class="ml-[5%] h-fit flex">
                         <div class="flex">
                             <div>
-                                <img src="https://lolcdn.darkintaqt.com/cdn/spells/{{$summonerData["game"][10]["ownData"]["summoner1Id"]}}" alt="rune" class="w-[35px] rounded-lg">
-                                <img src="https://lolcdn.darkintaqt.com/cdn/spells/{{$summonerData["game"][10]["ownData"]["summoner2Id"]}}" alt="rune" class="w-[35px] rounded-lg">
+                                <img src="https://lolcdn.darkintaqt.com/cdn/spells/{{ $summonerData['game'][10]['ownData']['summoner1Id'] }}"
+                                    alt="rune" class="w-[35px] rounded-lg">
+                                <img src="https://lolcdn.darkintaqt.com/cdn/spells/{{ $summonerData['game'][10]['ownData']['summoner2Id'] }}"
+                                    alt="rune" class="w-[35px] rounded-lg">
                             </div>
                         </div>
                         <div class="ml-[15px]">
-                            <h2 class="text-[20px]">{{$summonerData["game"][10]["ownData"]["kills"]}} / <span class="text-kda_red">{{$summonerData["game"][10]["ownData"]["deaths"]}}</span> / {{$summonerData["game"][10]["ownData"]["assists"]}}</h2>
-                            <p class="text-[16px] text-{{$color}}">{{$summonerData["game"][10]["ownData"]["kda"]}} KDA</p>
-                            <p class="text-[16px]">{{$summonerData["game"][10]["ownData"]["totalMinionsKilled"]}} CS ({{$summonerData["game"][10]["ownData"]["minionsPerMinute"]}})</p>
+                            <h2 class="text-[20px]">{{ $summonerData['game'][10]['ownData']['kills'] }} / <span
+                                    class="text-kda_red">{{ $summonerData['game'][10]['ownData']['deaths'] }}</span> /
+                                {{ $summonerData['game'][10]['ownData']['assists'] }}</h2>
+                            <p class="text-[16px] text-{{ $color }}">
+                                {{ $summonerData['game'][10]['ownData']['kda'] }} KDA</p>
+                            <p class="text-[16px]">{{ $summonerData['game'][10]['ownData']['totalMinionsKilled'] }} CS
+                                ({{ $summonerData['game'][10]['ownData']['minionsPerMinute'] }})</p>
                         </div>
                     </div>
                     <div class="flex flex-wrap h-[35%] mt-2 items-center justify-start">
-                        @foreach ($summonerData["game"][10]["ownData"]["items"] as $item)
-                            @if ($item!=0)
-                                <img src="https://ddragon.leagueoflegends.com/cdn/14.9.1/img/item/{{$item}}.png" alt="" class="h-[35px] rounded-lg">
+                        @foreach ($summonerData['game'][10]['ownData']['items'] as $item)
+                            @if ($item != 0)
+                                <img src="https://ddragon.leagueoflegends.com/cdn/14.9.1/img/item/{{ $item }}.png"
+                                    alt="" class="h-[35px] rounded-lg">
                             @endif
-                            @if ($item==0)
+                            @if ($item == 0)
                                 <img src="{{ asset('images/vacio.svg') }}" alt="" class="h-[35px]">
                             @endif
                         @endforeach
@@ -57,23 +66,30 @@
 
             <div class="flex justify-center">
                 <div class="m-6 w-[250px]">
-                    @for ($i=0;5>$i;$i++)
+                    @for ($i = 0; 5 > $i; $i++)
                         <div class="flex items-center my-[2px] justify-start">
-                            <img src="https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/{{$summonerData["game"][$i]["championId"]}}.png" class="w-[30px] rounded-lg">
-                            <p class="text-text_light text-ellipsis">{{$summonerData["game"][$i]["riotIdGameName"]}}#{{$summonerData["game"][$i]["riotIdTagline"]}}</p>
+                            <img src="https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/{{ $summonerData['game'][$i]['championId'] }}.png"
+                                class="w-[30px] rounded-lg">
+                            <p class="text-text_light text-ellipsis">
+                                {{ $summonerData['game'][$i]['riotIdGameName'] }}#{{ $summonerData['game'][$i]['riotIdTagline'] }}
+                            </p>
                         </div>
                     @endfor
                 </div>
                 <div class="m-6 w-[250px]">
-                    @for ($i=5;10>$i;$i++)
+                    @for ($i = 5; 10 > $i; $i++)
                         <div class="flex items-center my-[2px] justify-start">
-                            <img src="https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/{{$summonerData["game"][$i]["championId"]}}.png" class="w-[30px] rounded-lg">
-                            <p class="text-text_light text-ellipsis">{{$summonerData["game"][$i]["riotIdGameName"]}}#{{$summonerData["game"][$i]["riotIdTagline"]}}</p>
+                            <img src="https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/{{ $summonerData['game'][$i]['championId'] }}.png"
+                                class="w-[30px] rounded-lg">
+                            <p class="text-text_light text-ellipsis">
+                                {{ $summonerData['game'][$i]['riotIdGameName'] }}#{{ $summonerData['game'][$i]['riotIdTagline'] }}
+                            </p>
                         </div>
                     @endfor
                 </div>
             </div>
         </div>
-        <div class="w-8 {{$bgColor}} rounded-r-[10px] flex items-end cursor-pointer"><img src="{{ asset('images/flechaAbajo.svg') }}" alt="fecha abajo" class="w-full"></div>
+        <div class="w-8 {{ $bgColor }} rounded-r-[10px] flex items-end cursor-pointer"><img
+                src="{{ asset('images/flechaAbajo.svg') }}" alt="fecha abajo" class="w-full"></div>
     </div>
 @endif
--- resources/views/components/porcentaje-winrate.blade.php
+++ resources/views/components/porcentaje-winrate.blade.php
@@ -1,19 +1,19 @@
-@props(['class','summonerData'])
-{{-- @dd($summonerData) --}}
+@props(['class', 'summonerData'])
+
 @php
-    $wins=0;
-    $loses=0;
-    $kda=0;
+    $wins = 0;
+    $loses = 0;
+    $kda = 0;
     foreach ($summonerData as $champ) {
-        $wins+=$champ["totalWins"]+$champ["totalWins"];
-        $loses+=($champ["counts"]-$champ["totalWins"]);
-        $kda+=($champ["totalKDA"]+$champ["totalKDA"]);
+        $wins += $champ['totalWins'] + $champ['totalWins'];
+        $loses += $champ['counts'] - $champ['totalWins'];
+        $kda += $champ['totalKDA'] + $champ['totalKDA'];
     }
-    $kda=$kda/count($summonerData);
+    $kda = $kda / count($summonerData);
 @endphp
-<div class="border-solid border-2 border-primary rounded-[10px] flex flex-col bg-transparent text-text_light bg-opacity-60 bg-bg_dark h-fit {{ $class }}">
+<div
+    class="border-solid border-2 border-primary rounded-[10px] flex flex-col bg-transparent text-text_light bg-opacity-60 bg-bg_dark h-fit {{ $class }}">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    {{-- <div class="w-[1100px] h-[165px] border-primary border-solid border-[1px] rounded-[10px] flex flex-col bg-transparent text-white"> --}}
     <div class="flex justify-center p-4">
         <div class="flex flex-row justify-between w-[50%]">
             <x-performance-selector-button isSelected="true" name="Total" />
@@ -29,30 +29,32 @@
     <div class="flex flex-wrap md:flex-nowrap p-4 justify-between">
         <div class="flex flex-wrap justify-between items-center">
             <div class="flex flex-wrap sm:flex-nowrap gap-8">
-                <canvas id="PorcentajeWinRate" width="75" height="70" win="{{$wins}}" lose="{{$loses}}"></canvas>
+                <canvas id="PorcentajeWinRate" width="75" height="70" win="{{ $wins }}"
+                    lose="{{ $loses }}"></canvas>
                 <div class="flex flex-wrap items-center">
-                    <x-historial-wins-loses wins="{{$wins}}" loses="{{$loses}}" />
-                    <p class="text-lg font-bold w-[80px] ml-0 2xl:ml-8 {{ round($kda,3) }}">{{ round($kda,3) }} KDA</p>
+                    <x-historial-wins-loses wins="{{ $wins }}" loses="{{ $loses }}" />
+                    <p class="text-lg font-bold w-[80px] ml-0 2xl:ml-8 {{ round($kda, 3) }}">{{ round($kda, 3) }} KDA
+                    </p>
                 </div>
             </div>
-            {{-- <x-historial-k-d-a kda="{{($data->champ1->kda+$data->champ2->kda+$data->champ3->kda)/3}}"/> --}}
         </div>
         <div class="flex flex-wrap justify-center items-center">
             <div class="w-full h-fit mb-4 flex justify-between items-center font-semibold">
-                @for ($i=0;$i<3;$i++)
-                    {{-- @dd($summonerData[$i]) --}}
+                @for ($i = 0; $i < 3; $i++)
                     @if (isset($summonerData[$i]))
-                        {{-- @dump($summonerData[$i]) --}}
                         @php
-                                $championName=$summonerData[$i]['championName'];
-                                $totalKDA=$summonerData[$i]['totalKDA'];
-                                $winrate=round($summonerData[$i]['winrate'],2);
-                                $totalWins=$summonerData[$i]['totalWins'];
-                                $counts=$summonerData[$i]['counts'];
-                                $championId=$summonerData[$i]['championId'];
-                                $champLoses=$counts-$totalWins;
+                            $championName = $summonerData[$i]['championName'];
+                            $totalKDA = $summonerData[$i]['totalKDA'];
+                            $winrate = round($summonerData[$i]['winrate'], 2);
+                            $totalWins = $summonerData[$i]['totalWins'];
+                            $counts = $summonerData[$i]['counts'];
+                            $championId = $summonerData[$i]['championId'];
+                            $champLoses = $counts - $totalWins;
                         @endphp
-                        <x-historial-champ-stats champName="{{$championName}}" champKDA="{{$totalKDA}}" champWinRate="{{$winrate}}" champWins="{{$totalWins}}" champLoses="{{$champLoses}}" champImage="https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/{{$championId}}.png"/>
+                        <x-historial-champ-stats champName="{{ $championName }}" champKDA="{{ $totalKDA }}"
+                            champWinRate="{{ $winrate }}" champWins="{{ $totalWins }}"
+                            champLoses="{{ $champLoses }}"
+                            champImage="https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/{{ $championId }}.png" />
                     @endif
                 @endfor
             </div>
